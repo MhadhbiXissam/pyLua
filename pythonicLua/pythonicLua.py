360375f72d26c08e9272b4c04f8cdfb1aaaf0b77
Mhadhbi Issam <95243282+MhadhbiXissam@users.noreply.github.com>
2023-01-19 14:59:42 +0100

    Update pythonicLua.py

--- pythonicLua/pythonicLua.py
+++ pythonicLua/pythonicLua.py
@@ -1,4 +1,4 @@
-import sys , ast , os 
+import sys , ast , os , traceback
 from compiler import ast_globals
 
 def getEXPORT_FILE(file) : 
@@ -7,27 +7,41 @@ def getEXPORT_FILE(file) :
 		for line in pyfile : 
 			if line.startswith("###") :
 				EXPORT_FILE = line.strip()[3:]
-	return EXPORT_FILE
+	return EXPORT_FILE.strip()
 
 
 
 def transpile_code(file,export_file = None) : 
 	code = ""
 	with open(file) as buff :  code = ast.dump(ast.parse(buff.read()))
-	code_generated = eval("repr(" + code + ")",ast_globals)
-	code_generated = "		--lua code genrated from file : " + os.path.abspath(file) + "\n\n" + code_generated
+	code_generated  = "--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n"
+	code_generated += "--python file\t:\t" + os.path.abspath(file) +"\n" 
+	code_generated += "--python ast\t:\t" + code + "\n"
+	code_generated += "--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n"
+	has_error = False
+	try : 
+		code_generated += eval("repr(" + code + ")",ast_globals)
+	except Exception as e  : 
+		print("BAD PYTHON SYTHAXE :",traceback.print_exc())
+		print(code)
+		has_error = True
+	code_generated += "\n--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+	if not has_error : print(code_generated)
 	EXPORT_FILE = getEXPORT_FILE(file)
 	if export_file != None : 
 		EXPORT_FILE = export_file
 	if EXPORT_FILE == None :
-		print("generated code = \n" ,code_generated)
+		pass
 	else : 
 		print(code_generated , file = open(EXPORT_FILE , "w"))
-		print("generated file= \n" ,EXPORT_FILE)
+	return code
 
 
 if __name__ == '__main__':
-	if len(sys.argv) > 1  : transpile_code(sys.argv[1],export_file = None)
+	if len(sys.argv) > 1  : 
+		code = transpile_code(sys.argv[1],export_file = None)
+		print(sys.argv)
+		print("debug:" , code)
 
 
 
